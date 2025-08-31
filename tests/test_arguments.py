c9f8a94090b5c0014d5efb43436527ff35d81dab
Miyana Stange <mia.stange@outlook.com>
2025-08-31 21:30:59 +0200

    using the unittest assertion functions

--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -7,7 +7,7 @@ class TestArguments(unittest.TestCase):
     def test_ok(self):
         args = parse_args(['--name', 'M3oW'])
         app_obj = main(args)
-        assert str(app_obj) == 'Hello, this is the M3oW LTP app.'
+        self.assertEqual(str(app_obj), 'Hello, this is the M3oW LTP app.')
 
     def test_too_long(self):
         with self.assertRaises(SystemExit) as cm:
@@ -18,7 +18,7 @@ class TestArguments(unittest.TestCase):
     def test_missing(self):
         args = parse_args([])
         app_obj = main(args)
-        assert app_obj is not None
+        self.assertIsNotNone(app_obj)
 
 if __name__ == "__main__":
     unittest.main()
